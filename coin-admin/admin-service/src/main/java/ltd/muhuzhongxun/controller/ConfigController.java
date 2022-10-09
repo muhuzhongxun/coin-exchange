5d3787c7f16aa5639fb0c51a6be6847f2e0a5ee4
muhuzhongxun <983724494@qq.com>
2022-10-09 15:53:42 +0800

    后台参数管理

---
+++ coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/controller/ConfigController.java
@@ -0,0 +1,77 @@
+package ltd.muhuzhongxun.controller;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import ltd.muhuzhongxun.domain.AdminBank;
+import ltd.muhuzhongxun.domain.Config;
+import ltd.muhuzhongxun.model.R;
+import ltd.muhuzhongxun.service.AdminBankService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import ltd.muhuzhongxun.service.ConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequestMapping("/configs")
+@Api(tags = "后台参数配置")
+public class ConfigController {
+
+
+    @Autowired
+    private ConfigService configService ;
+
+
+    @GetMapping
+    @ApiOperation(value = "条件分页查询后台参数")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "type" ,value = "后台参数类型", dataTypeClass = String.class),
+            @ApiImplicitParam(name = "code" ,value = "后台参数code", dataTypeClass = String.class),
+            @ApiImplicitParam(name = "name" ,value = "后台参数名称", dataTypeClass = String.class),
+            @ApiImplicitParam(name = "current" ,value = "当前页", dataTypeClass = String.class),
+            @ApiImplicitParam(name = "size" ,value = "每页显示的条数", dataTypeClass = String.class),
+
+    })
+    @PreAuthorize("hasAuthority('config_query')")
+    public R<Page<Config>> findByPage(@ApiIgnore Page<Config> page , String type , String code , String name){
+        Page<Config> configPage = configService.findByPage(page,type,name,code) ;
+        return  R.ok(configPage) ;
+    }
+
+
+    @PostMapping
+    @ApiOperation(value = "新增一个参数")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "config",value = "config 的json数据", dataTypeClass = String.class)
+    })
+    @PreAuthorize("hasAuthority('config_create')")
+    public R add(@RequestBody @Validated  Config config){
+        boolean save = configService.save(config);
+        if(save){
+            return R.ok() ;
+        }
+        return R.fail("新增失败") ;
+    }
+
+
+
+    @PatchMapping
+    @ApiOperation(value = "修改一个参数")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "config",value = "config 的json数据", dataTypeClass = String.class)
+    })
+    @PreAuthorize("hasAuthority('config_update')")
+    public R update(@RequestBody @Validated  Config config){
+        boolean update = configService.updateById(config);
+        if(update){
+            return R.ok() ;
+        }
+        return R.fail("修改失败") ;
+    }
+
+}
--- coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/service/ConfigService.java
+++ coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/service/ConfigService.java
@@ -1,10 +1,23 @@
 package ltd.muhuzhongxun.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import ltd.muhuzhongxun.domain.Config;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-public interface ConfigService extends IService<Config> {
+public interface ConfigService extends IService<Config>{
 
 
+    /**
+     * 条件分页查询参数
+     * @param page
+     *分页参数
+     * @param type
+     * 类型
+     * @param name
+     * 参数名称
+     * @param code
+     * 参数Code
+     * @return
+     */
+    Page<Config> findByPage(Page<Config> page, String type, String name, String code);
 }
-
