a22a44735f667ab71b1f58e0f3ded54d9aa91d75
muhuzhongxun <983724494@qq.com>
2022-10-09 15:52:05 +0800

    后台资源配置

--- coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/service/WebConfigService.java
+++ coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/service/WebConfigService.java
@@ -1,10 +1,12 @@
 package ltd.muhuzhongxun.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import ltd.muhuzhongxun.domain.WebConfig;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface WebConfigService extends IService<WebConfig> {
 
 
+    Page<WebConfig> findByPage(Page<WebConfig> page, String name, String type);
 }
 
--- coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/service/impl/WebConfigServiceImpl.java
+++ coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/service/impl/WebConfigServiceImpl.java
@@ -1,15 +1,23 @@
 package ltd.muhuzhongxun.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
-import java.util.List;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import ltd.muhuzhongxun.mapper.WebConfigMapper;
 import ltd.muhuzhongxun.domain.WebConfig;
 import ltd.muhuzhongxun.service.WebConfigService;
+import org.springframework.util.StringUtils;
 
 @Service
 public class WebConfigServiceImpl extends ServiceImpl<WebConfigMapper, WebConfig> implements WebConfigService {
 
+    @Override
+    public Page<WebConfig> findByPage(Page<WebConfig> page, String name, String type) {
+        return page(page, new LambdaQueryWrapper<WebConfig>()
+                .like(!StringUtils.isEmpty(name),WebConfig::getName,name)
+                .eq(!StringUtils.isEmpty(type),WebConfig::getType,type)
+        );
+    }
 }
 
