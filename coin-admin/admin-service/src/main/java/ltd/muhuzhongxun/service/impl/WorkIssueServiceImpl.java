216e7bee6a699a17b97c1f19d6228b3e584cb563
muhuzhongxun <983724494@qq.com>
2022-10-09 15:52:49 +0800

    后台客服工单

--- coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/service/impl/WorkIssueServiceImpl.java
+++ coin-admin/admin-service/src/main/java/ltd/muhuzhongxun/service/impl/WorkIssueServiceImpl.java
@@ -1,5 +1,7 @@
 package ltd.muhuzhongxun.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
@@ -7,9 +9,28 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import ltd.muhuzhongxun.domain.WorkIssue;
 import ltd.muhuzhongxun.mapper.WorkIssueMapper;
 import ltd.muhuzhongxun.service.WorkIssueService;
+import org.springframework.util.StringUtils;
 
 @Service
-public class WorkIssueServiceImpl extends ServiceImpl<WorkIssueMapper, WorkIssue> implements WorkIssueService {
+public class WorkIssueServiceImpl extends ServiceImpl<WorkIssueMapper, WorkIssue> implements WorkIssueService{
 
+    /**
+     * 条件分页查询工单列表
+     *
+     * @param page      分页参数
+     * @param status    工单的状态
+     * @param startTime 查询的工单创建起始时间
+     * @param endTime   查询的工单创建截至时间
+     * @return
+     */
+    @Override
+    public Page<WorkIssue> findByPage(Page<WorkIssue> page, Integer status, String startTime, String endTime) {
+        return page(page, new LambdaQueryWrapper<WorkIssue>()
+                .eq(status!=null ,WorkIssue::getStatus,status)
+                .between(
+                        !StringUtils.isEmpty(startTime) && !StringUtils.isEmpty(endTime),
+                        WorkIssue::getCreated,
+                        startTime,endTime+" 23:59:59" )
+        );
+    }
 }
-
